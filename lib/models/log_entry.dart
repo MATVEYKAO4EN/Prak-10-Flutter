c8420f28022e99a8d469342d42028af9a9d9ecdb
MATVEYKAO4EN <matveykao4en@gmail.com>
2025-12-20 00:30:44 +0300

    update

--- lib/models/log_entry.dart
+++ lib/models/log_entry.dart
@@ -9,34 +9,6 @@ enum EntityType {
   document,
   product,
 }
-
-class LogEntry {
-  final LogAction action;
-  final EntityType entityType;
-  final String entityName;
-  final DateTime timestamp;
-  final Map<String, dynamic>? changes;
-
-  LogEntry({
-    required this.action,
-    required this.entityType,
-    required this.entityName,
-    required this.timestamp,
-    this.changes,
-  });
-
-  String get actionText {
-    switch (action) {
-      case LogAction.create:
-        return 'Создан';
-      case LogAction.update:
-        return 'Обновлен';
-      case LogAction.delete:
-        return 'Удален';
-    }
-  }
-
-  String get entityTypeText {
     switch (entityType) {
       case EntityType.document:
         return 'Документ';
